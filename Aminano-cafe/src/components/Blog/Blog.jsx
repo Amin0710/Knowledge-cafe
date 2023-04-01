3575aab6c328f7f421a19427ee272e48fd608a25
Nurul Amin <nurul.amin.naim@gmail.com>
2023-04-02 02:16:52 +1000

    tost works

--- Aminano-cafe/src/components/Blog/Blog.jsx
+++ Aminano-cafe/src/components/Blog/Blog.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import BlogItem from "../BlogItem/BlogItem";
 import "./Blog.css";
+import toast, { Toaster } from "react-hot-toast";
 
 const Blog = () => {
 	const [blogs, setBlogs] = useState([]);
-	const [list, setList] = useState([]);
+	const [time, setTime] = useState([]);
+	// const [list, setList] = useState([]);
 
 	useEffect(() => {
 		fetch("blogs.json")
@@ -13,29 +15,54 @@ const Blog = () => {
 	}, []);
 
 	const handleAddToList = (blog) => {
-		const newList = [...list, blog];
-		setList(newList);
+		const newTime = [...time, blog];
+		setTime(newTime);
+	};
+
+	const bookmarked = [];
+
+	const handleBookmark = (blog) => {
+		if (bookmarked.includes(blog.id)) {
+			toast.error("This didn't work. Already bookmarked", {
+				duration: 2000,
+			});
+		} else {
+			bookmarked.push(blog.id);
+		}
 	};
 
 	return (
 		<div className="blog-list-container">
+			<Toaster />
 			<div className="blog-container">
 				{blogs.map((blog) => (
 					<BlogItem
 						key={blog.id}
 						blog={blog}
-						handleAddToList={handleAddToList}></BlogItem>
+						handleAddToList={handleAddToList}
+						handleBookmark={handleBookmark}></BlogItem>
 				))}
 			</div>
 			<div className="record-container">
 				<div className="time-container">
 					<p className="record-header">
 						Spent time on read :{" "}
-						{list.map((l) => l.readTime).reduce((b, a) => b + a, 0)} min{" "}
+						{time.map((l) => l.readTime).reduce((b, a) => b + a, 0)} min{" "}
 					</p>
 				</div>
 				<div className="bookmark-container">
-					<p className="record-header">Bookmarked Blogs : 8 </p>
+					<p className="record-header">
+						Bookmarked Blogs : {bookmarked.length}{" "}
+					</p>
+					<div className="blog-container">
+						{bookmarked.map((id) => (
+							<BlogItem
+								key={blog.id}
+								blog={blog}
+								handleAddToList={handleAddToList}
+								handleBookmark={handleBookmark}></BlogItem>
+						))}
+					</div>
 				</div>
 			</div>
 		</div>
--- Aminano-cafe/src/components/BlogItem/BlogItem.jsx
+++ Aminano-cafe/src/components/BlogItem/BlogItem.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./BlogItem.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 
 const BlogItem = (props) => {
 	const { coverImg, authorImg, authorName, title, date, tags, readTime } =
 		props.blog;
 	const handleAddToList = props.handleAddToList;
+	const handleBookmark = props.handleBookmark;
 
 	return (
-		<div className="blog">
+		<div>
 			<img className="cover" src={coverImg} alt="" />
 			<div className="read-info">
 				<div className="author-info">
@@ -15,21 +18,27 @@ const BlogItem = (props) => {
 					<div>
 						<p style={{ fontSize: "24px" }}>{authorName}</p>
 						<p
-							style={{ fontSize: "16px", fontWeight: "600", color: "#969696" }}>
+							style={{
+								fontSize: "16px",
+								fontWeight: "600",
+								color: "#969696",
+							}}>
 							{date}
 						</p>
 					</div>
 				</div>
-				<p className="gray-small">{readTime} min read</p>
+				<p className="gray-small">
+					{readTime} min read{" "}
+					<i onClick={() => handleBookmark(props.blog)} className="icon">
+						<FontAwesomeIcon icon={faBookmark} />
+					</i>
+				</p>
 			</div>
 			<p style={{ padding: "10px 0" }}>{title}</p>
 			<p className="gray-small">
 				{tags[0]} {tags[1]} {tags[2]}
 			</p>
-			<button onClick={() => handleAddToList(props.blog)} className="btn-cart">
-				Mark as read
-			</button>
-
+			<a onClick={() => handleAddToList(props.blog)}>Mark as read</a>
 			<div style={{ border: "1px solid #969696", margin: "40px 0" }}></div>
 		</div>
 	);
